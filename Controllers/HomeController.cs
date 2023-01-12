24578fa94e749cacf11883eea0bbc0e1b5d90af0
gamejunkiez <queenangrylobster@gmail.com>
2023-01-12 12:34:45 -0600

    Ported Program To MVC

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using DumbItDown.Models;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI.GPT3.Interfaces;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using System.Diagnostics;
+using OpenAIModels = OpenAI.GPT3.ObjectModels;
+
+namespace DumbItDown.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IOpenAIService openAiService;
+
+
+        public HomeController(ILogger<HomeController> logger, IOpenAIService openAiService)
+        {
+            _logger = logger;
+            this.openAiService = openAiService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string query = "dogs")
+        {
+            var completionResult = await openAiService.Completions.CreateCompletion(new CompletionCreateRequest()
+            {
+                Prompt = $"Can you explain {query} in simple terms? 50 words or less.",
+                MaxTokens = 90
+            }, OpenAIModels.Models.TextDavinciV3);
+
+            if (completionResult.Successful)
+            {
+                return View(null, completionResult.Choices?.FirstOrDefault()?.Text);
+            }
+            else
+            {
+                // log error
+                return View();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+using LaserCatEyes.HttpClientListener;
+using OpenAI.GPT3;
+using OpenAI.GPT3.Extensions;
+using OpenAI.GPT3.Interfaces;
+using OpenAI.GPT3.Managers;
+using OpenAI.GPT3.ObjectModels;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+
+namespace DumbItDown
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Configuration.AddJsonFile("appsettings.json")
+            .AddUserSecrets<Program>();
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddOpenAIService();
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.Run();
+        }
+    }
+}
